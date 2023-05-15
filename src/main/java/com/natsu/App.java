a9d619e98ce2bcd224dd1db130d67773f8bae3f2
natsukry <lixins@live.cn>
2023-05-15 22:50:47 +0800

    stream collect

---
+++ src/main/java/com/natsu/App.java
@@ -0,0 +1,17 @@
+package com.natsu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Description:
+ * Author:natsukry
+ * Date:2023-03-30
+ * Time:20:44
+ */
+@SpringBootApplication
+public class App {
+    public static void main(String[] args) {
+        SpringApplication.run(App.class, args);
+    }
+}
---
+++ src/main/java/com/natsu/bean/Cat.java
@@ -0,0 +1,17 @@
+package com.natsu.bean;
+
+import lombok.Builder;
+import lombok.Data;
+
+/**
+ * @author : natsukry
+ * @create : 5/15/2023 9:48 PM
+ */
+@Data
+@Builder
+public class Cat {
+    private String age;
+    private String color;
+
+
+}
